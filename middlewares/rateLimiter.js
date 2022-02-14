503734a073fb52177bf2d2e56fb80d9ea7d18d71
harshmewada <harshmewada056@gmail.com>
2022-02-14 19:37:11 +0530

    Update rateLimiter.js

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 1 * 60 * 1000,
   max: 20,
   skipSuccessfulRequests: true,
 });
